dc3d2c46386fef472515b429cdd8f017b09d5ed6
digitalTrilunaire <tristan.laurent96@gmail.com>
2018-11-06 20:59:32 +0100

    Correct CMakeList and add test program.
    Correct the automatic link in CMakeList.
    It was just a synthax mistake. I also add a test program that use
    OpenCV and just open the picture that you provide it as an argument.

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,18 @@
-  #include <iostream>
+#include <iostream>
+#include "opencv2/highgui.hpp"
 
+using namespace cv;
 
-  int main(int argc, char const *argv[]) {
-    std::cout << "hello stylisator" << std::endl;
-    return 0;
+int main(int argc, char const *argv[]) {
+  Mat im;
+  if(argc == 2)
+    im = imread(argv[1],CV_LOAD_IMAGE_COLOR);
+  else{
+    std::cout << "Provide an picture path as an argument." << '\n';
+    std::cout << "Usage: " << argv[0] << " <pathToPicture>" << '\n';
+    return 1;
   }
+  imshow("test picture",im);
+  waitKey();
+  return 0;
+}
