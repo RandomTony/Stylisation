a8aebbe6c857564e475c42ddd46bcf75db1cf118
digitalTrilunaire <tristan.laurent96@gmail.com>
2019-05-08 20:04:52 +0200

    Remove third party library

--- lib/lime-master/tests/core/test_core.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-#if !GTEST_OS_WINDOWS
-    ::testing::FLAGS_gtest_death_test_style = "threadsafe";
-#endif
-    return RUN_ALL_TESTS();
-}
